483385fe60cbb157b5bd212325616028c02c69b9
SCHAEZEL <898931589@qq.com>
2022-11-23 00:57:11 +0800

    enormous changes
    1. a new script to draw function graph in scene
    2. a new Shader to color the block in the function graphs
    3. import URP(with none modifies)

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -7,9 +7,41 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab;
 
+    [SerializeField, Range(10, 100)]
+    int resolution = 100;
+    float step;
+    Vector3 scale;
+    Vector3 position;
+    List<Transform> objectPool;
     private void Awake()
     {
-
+        DrawGraph();
     }
+
+    private void Update()
+    {
+        // DrawGraph();
+    }
+
+    private void DrawGraph()
+    {
+        step = 2f / resolution;
+        scale = Vector3.one * step;
+        for (int i = 0; i < resolution; i++)
+        {
+            var x = (i + 0.5f) * step - 1f;
+            var y = Mathf.Pow(x, 3);
+            Transform point = Instantiate(pointPrefab);
+            point.position = new Vector3(x, y, 0);
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    // private void GetPoint()
+    // {
+    //     if (objectPool.Count == 0)
+
+    // }
 
 }
