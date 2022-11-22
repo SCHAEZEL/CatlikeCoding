a2bf3213b49909b045047589064f9ada3a50b276
SCHAEZEL <898931589@qq.com>
2022-11-22 23:29:26 +0800

    little adjustments

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    private void Awake()
+    {
+
+    }
+
+}
