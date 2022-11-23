7581bd2a7d6c1574b4b25901f6712083678a79c3
SCHAEZEL <898931589@qq.com>
2022-11-24 00:44:44 +0800

    import point pool and make them move like sin function!

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -6,42 +6,63 @@ public class Graph : MonoBehaviour
 {
     [SerializeField]
     Transform pointPrefab;
+    List<Transform> pointPool;
 
-    [SerializeField, Range(10, 100)]
-    int resolution = 100;
+    [SerializeField, Range(10, 300)]
+    int resolution = 50;
     float step;
     Vector3 scale;
     Vector3 position;
-    List<Transform> objectPool;
     private void Awake()
     {
-        DrawGraph();
+        pointPool = new List<Transform>(resolution);
+        DrawGraph(resolution);
     }
 
     private void Update()
     {
-        // DrawGraph();
+        DrawGraph(resolution);
     }
 
-    private void DrawGraph()
+    private void DrawGraph(int res)
     {
-        step = 2f / resolution;
+        FillThePool(res);
+        ClearGraph();
+
+        step = 2f / res;
         scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++)
+
+        float delta = Time.time;
+        for (int i = 0; i < res; i++)
         {
             var x = (i + 0.5f) * step - 1f;
-            var y = Mathf.Pow(x, 3);
-            Transform point = Instantiate(pointPrefab);
+            var y = Mathf.Sin(Mathf.PI * x + delta);
+            Transform point = pointPool[i];
             point.position = new Vector3(x, y, 0);
             point.localScale = scale;
             point.SetParent(transform, false);
         }
     }
 
-    // private void GetPoint()
-    // {
-    //     if (objectPool.Count == 0)
+    // 填满对象池
+    private void FillThePool(int num)
+    {
+        var poolCount = pointPool.Count;
+        if (poolCount < num)
+        {
+            var newNum = num - poolCount;
+            for (int i = 0; i < newNum; i++)
+            {
+                Transform go = Instantiate(pointPrefab);
+                go.name = string.Format("point_{0}", poolCount + i + 1);
+                pointPool.Add(go);
+            }
+        }
+    }
 
-    // }
-
+    private void ClearGraph()
+    {
+        foreach (Transform point in pointPool)
+            point.position = new Vector3(100 * resolution, 0, 0);
+    }
 }
